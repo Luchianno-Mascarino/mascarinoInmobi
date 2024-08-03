8c6e0ff454a1408070d6b7e92cca47d0758ea4eb
Luchianno <luchianno.dev@gmail.com>
2024-08-03 21:19:45 +0200

    complete sign in page funct

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -4,39 +4,37 @@ export const signup = (req, res) => {
     console.log(req.body)
 esto es para hacer la prueba de si funcionan mis routes
 }*/
-
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = new User({ username, email, password: hashedPassword })
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
-        await newUser.save()
-        res.status(201).json("User created")
+        await newUser.save();
+        res.status(201).json({ success: true, message: "User created" });
     } catch (error) {
         next(error);
     }
-       
 }
 
 export const signIn = async (req, res, next) => {
     const { email, password } = req.body;
-    try{
-        const validUser = await User.findOne({ email })
-        if (!validUser) return next(errorHandler(404, 'User not found'));
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return res.status(404).json({ success: false, message: 'User not found' });
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Wrong credential'))
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-        const { password: pass, ...rest } = validUser._doc
+        if (!validPassword) return res.status(401).json({ success: false, message: 'Wrong credentials' });
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
         res
             .cookie('access_token', token, { httpOnly: true })
             .status(200)
-            .json(rest)
-    } catch(error) {
-        next(error)
+            .json({ success: true, ...rest });
+    } catch (error) {
+        next(error);
     }
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    setError(null);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // to prevent refresh
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+      } else {
+        navigate('/'); // Redirigir solo si el inicio de sesión es exitoso
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>
-      Sign in
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+          value={formData.password}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      {error && <p className='text-red-500'>{error}</p>}
+      <div className='flex gap-2 mt-5'>
+        <p>Do not have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
