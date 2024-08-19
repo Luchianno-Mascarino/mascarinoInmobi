819413fdfa4b2b491ad9718058ab8770a4d5bdf2
Luchianno <luchianno.dev@gmail.com>
2024-08-19 19:42:59 +0200

    create user update api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routes/userRouter.js'
 import authRouter from './routes/authRouter.js'
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -15,7 +16,10 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+
 app.use(express.json())
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log("Server is running on 3000!!"); 
