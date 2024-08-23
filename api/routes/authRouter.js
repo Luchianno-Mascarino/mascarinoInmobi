849e6eb681477cc7ffdde464303853f39a27e66f
Luchianno <luchianno.dev@gmail.com>
2024-08-23 19:40:23 +0200

    add sign out functionality

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { google, signIn, signup } from '../controller/authController.js';
+import { google, signIn, signOut, signup } from '../controller/authController.js';
 
 const router = express.Router();
 
 router.post("/signup", signup)
 router.post("/signin", signIn)
 router.post('/google', google)
+router.get('/signout', signOut)
 
 export default router
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -89,6 +89,24 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message))
     }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+
+        return;
+      }
+
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
+    }
   }
 
   return (
@@ -117,7 +135,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
       <p className='text-red-700 mt-5' >{error ? error : ''}</p>
       <p className='text-green-700 mt-5' >{updateSuccess ? 'User is updated successfully' : ''}</p>
