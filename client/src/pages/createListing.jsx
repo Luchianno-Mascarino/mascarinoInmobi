38376bb64d0b280664c77fcee7a08856f146a950
Luchianno <luchianno.dev@gmail.com>
2024-08-26 20:42:06 +0200

    add ENV Examples

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from "react";
+
 
 export default function CreateListing() {
+    const [files, setFiles] = useState([]);
+    console.log(files);
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7' >Create a Listing</h1>
@@ -66,7 +69,7 @@ export default function CreateListing() {
                     <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
                 </p>
                 <div className='flex gap-4'>
-                    <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
+                    <input onChange={(e)=> setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
                     <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
                 </div>
                 <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity95 disabled:opacity-80'>Create Listing</button>
