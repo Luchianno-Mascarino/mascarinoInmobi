84ada60a8429ab92fa014072a2393a3fe79b77b1
Luchianno <luchianno.dev@gmail.com>
2024-09-28 19:10:04 +0200

    prev about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      about
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Sobre Mascarino Inmobiliaria</h1>
+      <p className='mb-4 text-slate-700'>Escribir lo que queremos poner aca</p>
+      <p>lo mismo aca ir agreegando cosas</p>
     </div>
   )
 }
