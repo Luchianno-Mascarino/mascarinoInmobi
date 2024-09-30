2111190d2b1db2935170c1f2ee1946304396df3a
Luchianno <luchianno.dev@gmail.com>
2024-09-30 18:10:04 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRouter.js'
 import authRouter from './routes/authRouter.js'
 import listingRouter from './routes/listingRouter.js'
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 
@@ -15,7 +16,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 
+    const __dirname = path.resolve();    
+
 const app = express();
+
+
 
 
 app.use(express.json())
@@ -29,6 +34,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter)
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'/client/dist', 'index.html'));
+})
 
 //Middleware to handle errors
 app.use((err, req, res, next) => {
